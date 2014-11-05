7666fef5ea6c7f0593a04ac04189da30549e48b5
sybiload <sybiload@gmail.com>
2014-11-05 22:38:21 +0100

    Added a path finding by using NavMeshAgent and BakeNavigation

---
+++ Assets/Scripts/NavMesh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMesh : MonoBehaviour {
+
+	public Transform leader;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<NavMeshAgent> ().destination = leader.position;
+	}
+}
